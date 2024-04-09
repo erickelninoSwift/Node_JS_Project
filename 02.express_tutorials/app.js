fb08cc17e0c457f66b0918f6a61a0a8dd2d91e39
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-09 17:18:58 +0200

    sending our response with html pages and adjusting content-types

--- 02.express_tutorials/app.js
+++ 02.express_tutorials/app.js
@@ -1,7 +1,36 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html", "utf8");
+const homeStyle = readFileSync("./navbar-app/styles.css", "utf8");
+const homeLogo = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js", "utf8");
 
 const server = http.createServer((request, response) => {
-  response.end("<h1>Welcome to our Website </h1>");
+  if (request.url === "/") {
+    response.writeHead(200, {
+      "content-type": "text/html",
+    });
+
+    response.write(homePage);
+    response.end();
+  } else if (request.url === "/styles.css") {
+    response.writeHead(200, {
+      "content-type": "text/css",
+    });
+    response.end(homeStyle);
+  } else if (request.url === "/browser-app.js") {
+    response.writeHead(200, {
+      "content-type": "text/script",
+    });
+    response.end(homeLogic);
+  } else if (request.url === "/logo.svg") {
+    response.writeHead(200, {
+      "content-type": "image/svg+xml",
+    });
+    response.end(homeLogo);
+  }
+  console.log(request.url);
 });
 
 server.listen(8080, () => {

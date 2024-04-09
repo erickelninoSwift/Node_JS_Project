61fd54a1bd05104108f08bbbf6400f99d6157e97
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-09 04:09:47 +0200

    created a small server with the http method

--- 02.express_tutorials/app.js
+++ 02.express_tutorials/app.js
@@ -1,1 +1,9 @@
-console.log('Express Tutorial')
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+  response.end("<h1>Welcome to our Website </h1>");
+});
+
+server.listen(8080, () => {
+  console.log("Server is running well");
+});

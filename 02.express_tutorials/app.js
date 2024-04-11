58c4ee8f897fa891dd53031c6d5912ad93ff9bf4
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-11 03:12:49 +0200

    hotfix

--- 02.express_tutorials/app.js
+++ 02.express_tutorials/app.js
@@ -44,9 +44,56 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 app.get("/", (request, response) => {
-  response.json({ message: " jackpot here" });
+  response.send("<h1>Home Page </h1> <a href='/api/products'>product</a>");
+});
+
+// app.get("/api/products", (request, response) => {
+//   const newproducts = products.map((product) => {
+//     const { id, name, image } = product;
+//     return {
+//       id,
+//       name,
+//       image,
+//     };
+//   });
+
+// app.get("/api/products/:id", (request, response) => {
+//   const { id } = request.params;
+//   console.log(request.params);
+//   const productSelected = products.find((product) => product.id === Number(id));
+//   if (!productSelected) {
+//     response.status(404).send("Product was not found ");
+//   }
+//   return response.json(productSelected);
+// });
+
+app.get("/api/products/:productID/review/:reviewID", (request, response) => {
+  const { productID, reviewID } = request.params;
+
+  const productSelected = products.find((product) => product.id === Number(id));
+  if (!productSelected) {
+    response.status(404).send("Product was not found ");
+  }
+  return response.json(productSelected);
+});
+
+app.get("/api/v1/query", (request, response) => {
+  console.log(request.query);
+  const selectedData = [...products];
+  console.log(selectedData);
+  const { name, limit } = request.query;
+  const soetedSelcted = selectedData.filter((datafetched) => {
+    return datafetched.name.startsWith(name);
+  });
+
+  if (limit) {
+    const sortedWithLimit = soetedSelcted.slice(0, Number(limit));
+    response.status(200).json(sortedWithLimit);
+  } else {
+    response.status(200).json(soetedSelcted);
+  }
 });
 
 app.listen(PORT, () => {

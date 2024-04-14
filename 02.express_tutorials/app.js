b609b1aa33d2af9f854da01fc652ab3392f482fc
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-14 02:48:43 +0200

    hotfix

--- 02.express_tutorials/app.js
+++ 02.express_tutorials/app.js
@@ -5,6 +5,31 @@ const express = require("express");
 const PORT = 8080;
 const app = express();
 
+app.use(express.static("./methods-public"));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.get("/api/people", (request, response) => {
+  response.status(200).send({
+    success: true,
+    data: people,
+  });
+});
+
+app.post("/login", (request, response) => {
+  const { name } = request.body;
+
+  if (name) {
+    return response.status(200).json({
+      name: name,
+    });
+  }
+
+  response.send("Please do provide the credentials");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on Server  : ${PORT}`);
 });

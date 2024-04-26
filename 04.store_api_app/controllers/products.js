c7786a1b0a411ba48a25c5cc527d81bd31cf3947
Erick El nino <erick.tshimbombo@yahoo.com>
2024-04-26 03:08:35 +0200

    update new filtering methods

--- 04.store_api_app/controllers/products.js
+++ 04.store_api_app/controllers/products.js
@@ -1,13 +1,35 @@
-const getAllproducts = async (request, response) => {
-  response.status(200).json({
-    message: "Get All products",
-  });
-};
+const { errorWrapper } = require("../middleware/erroWrapper");
+const allProductsDB = require("../models/product");
 
-const getAllProductStatic = async (request, response) => {
+const getAllproducts = errorWrapper(async (request, response) => {
+  const { company, featured, name } = request.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? false : true;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const datafetched = await allProductsDB.find(queryObject);
+
   response.status(200).json({
-    message: "Get All products Static",
+    status: "success",
+    products: datafetched,
+    totlaClick: datafetched.length,
   });
-};
+});
+
+const getAllProductStatic = errorWrapper(async (request, response) => {
+  const datafetched = await allProductsDB.find({});
+  response.status(200).json({
+    message: datafetched,
+  });
+});
 
 module.exports = { getAllproducts, getAllProductStatic };
